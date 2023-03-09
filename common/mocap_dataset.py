d0ad0c28ce39c0b6572ee757a206a403cb74d321
rodrigocastillogl <rodrigocastillogl@gmail.com>
2023-03-08 18:15:37 -0600

    feat: add downsample() and _mirror_sequence() method to MocapDataset class (mocap_dataset.py)
    Copy methods from original repository.

--- common/mocap_dataset.py
+++ common/mocap_dataset.py
@@ -74,7 +74,7 @@ class MocapDataset:
         Read datset and return it as a dictionary.
         Input
         -----
-            * path : dataet path (.npz file format)
+            * path : dataset path (.npz file format)
         Output
         ------
             * result : Nested dictionaries
@@ -105,3 +105,76 @@ class MocapDataset:
             }
         
         return result
+    
+    def downsample(self, factor, keep_strides = True):
+        """
+        Downsample data by an integer factor, keeping all strides of the data
+        if keep_strides == True.
+        The sequences will be replaced by their downsampled version, whose actions
+        will have '_d0', ..., '_dn' appened to their name
+        Input
+        -----
+            * factor : downsamplig ratio (fps must be divisible by the factor).
+            * keep_strides : flag
+        Output
+        ------
+            None (in-place operator)
+        """
+
+        assert self._fps % factor == 0
+
+        # for every subject
+        for subject in self._data.keys():
+            new_actions = {}
+
+            # for every action
+            for action in list( subject.keys() ):
+
+                # to keep all strides
+                for idx in range(factor):
+                    tup = {}
+
+                    # for rotations and trayectory
+                    for k in self._data[subject][action].key():
+                        tup[k] = self._data[subject][action][k][idx::factor]
+                    
+                    new_actions[ action + '_d' + str(idx) ] = tup
+                    
+                    # In case we do not not want all strides
+                    if not keep_strides:
+                        break
+            
+            # replace subject actions
+            self._data[subject] = new_actions
+        
+        # update sampling rate
+        self._fps //= factor
+
+    def _mirror_sequence(self, sequence):
+        """
+        Description
+        Input
+        -----
+            * sequence : 
+        Output
+        ------
+            mirror sequence
+        """
+
+        mirrored_rotations  = sequence['rotations'].copy()
+        mirrored_trajectory = sequence['trajectory'].copy()
+
+        joints_left = self._skeleton.joints_left()
+        joints_right = self._skeleton.joints_right()
+
+        # flip left/right joints
+        mirrored_rotations[:, joints_left] = sequence['rotations'][:, joints_right]
+        mirrored_rotations[:, joints_right] = sequence['rotations'][:, joints_left]
+
+        mirrored_rotations[:, :, [2,3]] *= -1
+        mirrored_trajectory[:, 0]*= -1
+
+        return {
+            'rotations' : qfix(mirrored_rotations),
+            'trajectory': mirrored_trajectory
+        }
