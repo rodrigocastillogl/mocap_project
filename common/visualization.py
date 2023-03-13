0b3b295636e860c8d63434f29174253f64ea61b2
rodrigocastillogl <rodrigocastillogl@gmail.com>
2023-03-13 12:35:48 -0600

    feat: Complete render_animation() function (visualization.py).
    Copy complete function from original repository.

--- common/visualization.py
+++ common/visualization.py
@@ -60,4 +60,101 @@ def render_animation(data, skeleton, fps, output = 'interactive', bitrate = 1000
     initilized = False
 
     trajectory = data[:, 0, [0,2]]
+    avg_segment_length = np.mean(
+        np.linalg.norm( np.diff(trajectory, axis = 0), axis = 1) + 1e-3
+    )
+    draw_offset = int( 25 / avg_segment_length)
+    spline_line, = ax.plot(*trajectory.T)
+    camera_pos = trajectory
+    height_offset = np.min(data[:, :, 1])
+    data = data.copy()
+    data[:,:, 1] -= height_offset
 
+    def update(frame):
+        """
+        Description
+        Input
+        -----
+            * frame :
+        Output
+        ------
+            None
+        """
+
+        nonlocal initilized
+
+        # set axes limitis according to trajectory (to camera position)
+        ax.set_xlim3d(
+            [ -radius/2 + camera_pos[frame, 0], radius/2 + camera_pos[frame, 0] ]
+        )
+        ax.set_ylim3d(
+            [ -radius/2 + camera_pos[frame, 1], radius/2 + camera_pos[frame, 1] ]
+        )
+
+        positions_world = data[frame]
+        for i in range(positions_world.shape[0]):
+            if skeleton_parents[i] == -1:
+                continue
+            if not initilized:
+                # to distinguish between right and left joints 
+                col = 'red' if i in skeleton.joints_right() else 'black'
+                # draw skeleton
+                lines.append(
+                    ax.plot(
+                    [positions_world[i, x], positions_world[skeleton_parents[i], x]],
+                    [positions_world[i, y], positions_world[skeleton_parents[i], y]],
+                    [positions_world[i, z], positions_world[skeleton_parents[i], z]],
+                    zdir = 'y', c = col
+                    )
+                )
+            else:
+                lines[i-1][0].set_xdata(
+                    [ positions_world[i, x], positions_world[skeleton_parents[i], x] ]
+                )
+                lines[i-1][0].set_ydata(
+                    [ positions_world[i, y], positions_world[skeleton_parents[i], y] ]
+                )
+                lines[i-1][0].set_3d_properties(
+                    [ positions_world[i, z], positions_world[skeleton_parents[i], z] ],
+                    zdir = 'y'
+                )
+            
+            l = max( frame - draw_offset, 0)
+            r = min( frame+draw_offset, trajectory.shape[0] )
+
+            spline_line.set_xdata( trajectory[l:r, 0] )
+            spline_line.set_ydata( np.zeros_like(trajectory[l:r, 0]) )
+            spline_line.set_3d_properties( trajectory[l:r, 1], zdir = 'y')
+
+            initilized = True
+
+            # for interactive mode: if we get to the final frame, then close all the figures
+            if output == 'interactive' and frame == data.shape[0] - 1:
+                plt.close('all')
+            
+            fig.tight_layout()
+            anim = FuncAnimation(
+                fig    ,
+                update ,
+                frames = np.arange(0, data.shape[0]),
+                interval = 1000/fps ,
+                repeat = False
+            )
+
+            if output == 'interactive':
+                plt.show()
+                return anim
+            elif output == 'html':
+                return anim.to_html5_video()
+            elif output.endswith('.mp4'):
+                Writer = writers['ffmpeg']
+                writer = Writer(fps = fps, metadata={}, bitrate=bitrate)
+                anim.save( output, writer = writer )
+            elif output.endswith('.gif'):
+                anim.save(output, dpi=80, writer='imagemagick')
+            else:
+                raise ValueError(
+                    'Unsupported output format (only interactive,  html, .mp4 and .gif)'
+                )
+            
+            plt.close()
