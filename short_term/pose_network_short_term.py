ecc98dc059ff09b10ecb7241f670f89565da85ed
rodrigocastillogl <rodrigocastillogl@gmail.com>
2023-03-17 09:57:43 -0600

    feat: add short_term/pose_network_short_term.py.

---
+++ short_term/pose_network_short_term.py
@@ -0,0 +1,4 @@
+# ---------------------------------------------- #
+# Copyright (c) 2018-present, Facebook, Inc.
+# https://github.com/facebookresearch/QuaterNet
+# ---------------------------------------------- #
