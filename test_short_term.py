80825c3c72d583afbcd24454ff96fe130043606d
rodrigocastillogl <rodrigocastillogl@gmail.com>
2023-03-22 11:33:52 -0600

    feat: add test_short_term.py file.

---
+++ test_short_term.py
@@ -0,0 +1,5 @@
+# ---------------------------------------------- #
+# Copyright (c) 2018-present, Facebook, Inc.
+# https://github.com/facebookresearch/QuaterNet
+# ---------------------------------------------- #
+
