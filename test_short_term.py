40fb4e1ea48189c2ec7a5fa7f950934d8314b320
rodrigocastillogl <rodrigocastillogl@gmail.com>
2023-05-15 16:26:43 -0600

    test: change test line.

--- test_short_term.py
+++ test_short_term.py
@@ -136,7 +136,7 @@ def evaluate(model, test_data):
                 np.expand_dims(source, 0), target_length = np.max(frame_targets) + 1
             ).reshape(-1, 32*4)
         
-        print(target_predicted[0,0])
+        print(source[0,:3])
         target = qeuler_np( target[:target_predicted.shape[0]].reshape(-1,4), 'zyx' ).reshape(-1, 96)
         target_predicted = qeuler_np( target_predicted.reshape(-1,4), 'zyx').reshape(-1,96)
         e = np.sqrt( np.sum( (target_predicted[:,3:] - target[:,3:] )**2, axis = 1 ) )
