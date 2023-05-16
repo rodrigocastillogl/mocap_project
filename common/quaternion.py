092734305d48b2694d6af19765b598713499ecfa
rodrigocastillogl <rodrigocastillogl@gmail.com>
2023-05-16 11:53:00 -0600

    feat: change euler_to_quaternion() in quaternion.py

--- common/quaternion.py
+++ common/quaternion.py
@@ -252,9 +252,8 @@ def expmap_to_quaternion(e):
 
     return np.concatenate( (w, xyz), axis = 1 ).reshape(original_shape)
 
-
+"""
 def euler_to_quaternion(e, order):
-    """
     Convertion euler angles to quaternions.
     Fromula from:
     "Practical Parameterization of Rotations  Using the Exponential Map"
@@ -267,7 +266,6 @@ def euler_to_quaternion(e, order):
     Output
     ------
         * numpy array with dimensions (N, 4); quaternions
-    """
 
     assert e.shape[-1] == 3
 
@@ -305,3 +303,44 @@ def euler_to_quaternion(e, order):
             result *= -1
         
         return result.reshape(original_shape)
+"""
+
+def euler_to_quaternion(e, order):
+    """
+    Convert Euler angles to quaternions.
+    """
+    assert e.shape[-1] == 3
+    
+    original_shape = list(e.shape)
+    original_shape[-1] = 4
+    
+    e = e.reshape(-1, 3)
+    
+    x = e[:, 0]
+    y = e[:, 1]
+    z = e[:, 2]
+    
+    rx = np.stack((np.cos(x/2), np.sin(x/2), np.zeros_like(x), np.zeros_like(x)), axis=1)
+    ry = np.stack((np.cos(y/2), np.zeros_like(y), np.sin(y/2), np.zeros_like(y)), axis=1)
+    rz = np.stack((np.cos(z/2), np.zeros_like(z), np.zeros_like(z), np.sin(z/2)), axis=1)
+
+    result = None
+    for coord in order:
+        if coord == 'x':
+            r = rx
+        elif coord == 'y':
+            r = ry
+        elif coord == 'z':
+            r = rz
+        else:
+            raise
+        if result is None:
+            result = r
+        else:
+            result = qmul_np(result, r)
+            
+    # Reverse antipodal representation to have a non-negative "w"
+    if order in ['xyz', 'yzx', 'zxy']:
+        result *= -1
+    
+    return result.reshape(original_shape)
