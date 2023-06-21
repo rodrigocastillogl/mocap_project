ff28370fb8c8e6c08c822c02a12f4dbda4eb52b9
rodrigocastillogl <rodrigocastillogl@gmail.com>
2023-06-21 15:57:30 -0600

    docs: review code and add comments.

--- common/pose_network.py
+++ common/pose_network.py
@@ -14,7 +14,7 @@ from time import time
 
 class PoseNetwork:
     """
-    Description
+    Recurrent model to predict skeleton movement.
     Attributes
     ----------
         * translations_size : (num_outputs) extra inputs/outputs, in addition to joint rotations.
@@ -24,7 +24,7 @@ class PoseNetwork:
                               instead of absolute rotations.
         * model    : Quaternet model.
         * use_cuda : flag to use CUDA.
-        * prefix_length : ...
+        * prefix_length : Number of frames in the input. 
 
     Methods
     -------
@@ -44,33 +44,35 @@ class PoseNetwork:
         Initializer
         Input
         -----
-            * prefix_length :
+            * prefix_length : Number of frames in the input. 
             * num_joints    : number of skeleton joints
             * num_outputs   : extra inputs/outputs, in addition to joint rotations 
             * num_controls  : extra input features
             * model_velocities : flag to add a queternion multiplication block on the
-                             RNN output to force the network to model velocities
-                             instead of absolute rotations.
+                                 RNN output to force the network to model velocities
+                                 instead of absolute rotations.
         Output
         ------
             * None
         """
 
-        # initialize attributes
+       # ------------ PoseNetwork attributes ------------
         self.translations_size = num_outputs
         self.controls_size = num_controls
         self.model_velocities = model_velocities
         self.use_cuda = False
         self.prefix_length = prefix_length
+        # -----------------------------------------------
 
         # QuaterNet model
         self.model = QuaterNet( num_joints, num_outputs, num_controls, model_velocities )
 
-        # count and display the number of parameters in the model
+        # ------ Number of parameters in the model ------
         dec_params = 0
         for parameter in self.model.parameters():
             dec_params += parameter.numel()
         print('# parameters:', dec_params)
+        # -----------------------------------------------
 
     
     def cuda(self):
@@ -108,14 +110,12 @@ class PoseNetwork:
         """"
         This method must be implemented in the subclass
         """
-
         pass
 
     def _loss_impl( self, predicted, expected ):
         """
         This method must be implemented in the subclass
         """
-
         pass
 
 
@@ -125,12 +125,12 @@ class PoseNetwork:
         Train the model, updating parameters.
         Input
         -----
-            * dataset : dataset object
-            * target_length   : number of frames to forecast
-            * sequences_train : sequences used to train
+            * dataset : dataset object.
+            * target_length   : number of frames to forecast.
+            * sequences_train : sequences used to train.
             * sequences_valid : sequences used for validation
-            * batch_size : batch size during training
-            * n_epoch : number of epochs during training
+            * batch_size : batch size during training.
+            * n_epoch : number of epochs during training.
             * rot_reg : regularization parameter to force quaterions to be unitary.
         Output
         ------
@@ -144,15 +144,17 @@ class PoseNetwork:
         # set model in training mode
         self.model.train()
 
-        lr = 0.001                # learning rate
+        # -------------- Hyperparameters ----------------
+        lr = 0.001                 # learning rate
         lr_decay = 0.999           # learning rate decay factor
         batch_size_valid = 30      # batch size during validation step
         teacher_forcing_ratio = 1  # starts by forcing the ground truth 
         tf_decay = 0.995           # teacher forcing decay factor
         gradient_clip = 0.1        # gradient_clipping factor
-
         optimizer = optim.Adam( self.model.parameters(), lr = lr )
+        # -----------------------------------------------
 
+        # ---------------- Validation set ---------------
         if len(sequences_valid) > 0 :
             batch_in_valid, batch_out_valid = next(
                 self._prepare_next_batch_impl(batch_size_valid, dataset, target_length, sequences_valid)
@@ -160,12 +162,13 @@ class PoseNetwork:
             inputs_valid = torch.from_numpy(batch_in_valid)
             outputs_valid = torch.from_numpy(batch_out_valid)
 
-            # send to CUDA
             if self.use_cuda:
                 inputs_valid = inputs_valid.cuda()
                 outputs_valid = outputs_valid.cuda()
+        # -----------------------------------------------
 
-        # Start training    
+        # Start ---------------- Training ---------------
+
         losses = []             # training loss per epoch
         valid_losses = []       # validation loss per epoch
         gradient_norms = []     # gradient norm per epoch
@@ -182,35 +185,41 @@ class PoseNetwork:
 
                 for batch_in, batch_out in self._prepare_next_batch_impl(
                     batch_size,  dataset, target_length, sequences_train):
-   
+
+
+                    # --------------- Training batch ----------------
                     inputs = torch.from_numpy(batch_in)
                     outputs = torch.from_numpy(batch_out)
                     if self.use_cuda:
                         inputs = inputs.cuda()
                         outputs = outputs.cuda()
+                    # -----------------------------------------------
                         
                     optimizer.zero_grad()
                     terms = []
                     predictions = []
 
-                    # forward propagation
+                    # ------------- Forward propagation -------------
+                        
                         # x = inputs[:, :self.prefix_length]
                         # h = None
                         # return_prenorm = True
                         # return_all = False (default)
+
                     predicted , hidden, term = self.model(
                         inputs[:, :self.prefix_length], None, True
                     )
-                    
-                    # terms = prenormalized
+
                     terms.append(term)
                     predictions.append(predicted)
 
                     tf_mask = np.random.uniform(size = target_length-1) < teacher_forcing_ratio
                     i = 0
                     while i < target_length-1:
+
                         contiguous_frames = 1
-                        # Batch together consecutive "teacher forcings" to improve performance
+
+                        # ---------- Teacher forcings strategy ----------
                         if tf_mask[i]:
                             while (i + contiguous_frames) < (target_length - 1) and tf_mask[ i + contiguous_frames ]:
                                 contiguous_frames += 1
@@ -218,7 +227,6 @@ class PoseNetwork:
                             predicted, hidden, term = self.model(
                                     inputs[:, self.prefix_length+i:self.prefix_length+i+contiguous_frames], hidden, True, True
                             )
-
                         else:
                             # feed own output
                             if self.controls_size > 0:
@@ -226,6 +234,7 @@ class PoseNetwork:
                                     predicted, inputs[:, self.prefix_length+i:self.prefix_length+i+1, -self.controls_size:]
                                 ), dim = 2 )
                             predicted, hidden, term = self.model(predicted, hidden, True)
+                        # -----------------------------------------------
 
                         terms.append(term)
                         predictions.append(predicted)
@@ -238,27 +247,28 @@ class PoseNetwork:
                     terms = torch.cat(terms, dim = 1)
                     terms =  terms.view(terms. shape[0], terms.shape[1], -1 , 4)
 
-                    # Regulation term
+                    # -----------------------------------------------
+
+                    # -------- Compute loss & Backpropagation -------
                     penalty_loss = rot_reg * torch.mean(
                         ( torch.sum(terms**2, dim = 3) - 1 )**2
                     )
-
                     predictions = torch.cat(predictions, dim = 1)
                     loss = self._loss_impl(predictions, outputs)
-
                     loss_total = penalty_loss + loss
+
                     loss_total.backward()
                     nn.utils.clip_grad_norm_(self.model.parameters(), gradient_clip)
                     optimizer.step()
+                    # -----------------------------------------------
 
-                    # compute statisctics
                     batch_loss += loss.item() * inputs.shape[0]
                     N += inputs.shape[0]
 
                 batch_loss = batch_loss/N
                 losses.append(batch_loss)
 
-                # Validation
+                # ----------------- Validation ------------------
                 if len(sequences_valid) > 0:
                     with torch.no_grad():
                         predictions = []
@@ -288,25 +298,30 @@ class PoseNetwork:
                     print(
                         '[%d] loss: %.5f lr %f tf_ratio %f' % (epoch + 1, batch_loss, lr, teacher_forcing_ratio)
                     )
-                    
+                # -----------------------------------------------
+
+                # -------------- Update aparameters -------------
                 teacher_forcing_ratio *= tf_decay
                 lr *= lr_decay
-
                 for param_group in optimizer.param_groups:
                     param_group['lr'] *= lr_decay
-                    
-                if epoch > 0 and (epoch + 1) % 10 == 0:
+                # -----------------------------------------------
+                
+                # --------------- Display progress --------------
+                if epoch > 0 and (epoch + 1) % 20 == 0:
                     next_time = time()
                     time_per_epoch = (next_time - start_time) / (epoch - start_epoch)
                     print('Benchmark:', time_per_epoch, 's per epoch')
                     start_time = next_time
                     start_epoch = epoch
+                # -----------------------------------------------
 
         except KeyboardInterrupt:
             print('Training aborted.')
             
+        # End ----------------- Training ----------------
+            
         print('Done.')
-
         return losses, valid_losses, gradient_norms
     
 
