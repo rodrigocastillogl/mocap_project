fix: correct computation of mean error per joint.

test_short_term.py
@@ -141,9 +141,9 @@ def evaluate(model, test_data):
         target = qeuler_np( target[:target_predicted.shape[0]].reshape(-1,4), 'zyx' ).reshape(-1, model.num_joints, 3)
         target_predicted = qeuler_np( target_predicted.reshape(-1,4), 'zyx').reshape(-1, model.num_joints, 3)
         
-        mean_error_joint = np.sum( (target_predicted - target)**2, axis = 1 )
+        mean_error_joint = np.sum( (target_predicted - target)**2, axis = 2 )
         print(mean_error_joint.shape)
-        
+
         e = np.sqrt( np.sum( (target_predicted.reshape(-1, model.num_joints*3)[:,3:] - target.reshape(-1, model.num_joints*3)[:,3:] )**2, axis = 1 ) )
         errors.append(e)
     errors = np.mean( np.array(errors), axis = 0 )